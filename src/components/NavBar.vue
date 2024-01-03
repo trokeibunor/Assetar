aa29f9c4da25a8e07bfaadc224262f1f3756675b
Okeibunor Emmanuel <okeibunoremma@gmail.com>
2024-01-03 12:11:57 +0100

    new design

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,157 +1,66 @@
 <template>
   <nav>
-    <div class="content">
-      <img id="logo" src="../assets/images/logo.png" alt="logoImage" />
-      <div class="navMenu">
-        <a @click="routeTo('/')">Home</a>
-        <a @click="routeTo('/#token')">Token</a>
-        <a @click="routeTo('about')">Our Story</a>
-        <a @click="routeTo('/#contact')" id="conBtn">Contact Us</a>
+    <div class="top-info">
+      <p>Trade your coins and tokens crazy speed</p>
+    </div>
+    <div class="nav-content">
+      <div class="logo">
+        <img src="../assets/images//logo.png" alt="" />
+        <p>Asseter</p>
       </div>
-      <!-- Mobile toggler -->
-      <div class="navToggler">
-        <button v-show="isNavOpen" @click="isNavOpen = false">
-          <!-- <span class="material-symbols-outlined"> menu </span> -->
-          <img src="../assets/images/menu_hamburger.svg" alt="" srcset="" />
-        </button>
-        <button v-show="!isNavOpen" @click="isNavOpen = true">
-          <!-- <span class="material-symbols-outlined"> close </span> -->
-          <img src="../assets/images/cancel_icon.svg" alt="" srcset="" />
-        </button>
+      <div class="nav-list">
+        <p>About Us</p>
+        <p>Testimonials</p>
+        <p>Contact us</p>
+      </div>
+      <div class="buttons">
+        <p>Socials</p>
+        <button>Get Started</button>
       </div>
     </div>
   </nav>
-  <aside v-if="!isNavOpen">
-    <!-- logo -->
-    <!-- sideMenu -->
-    <div class="sidemenu" v-if="isNavOpen == false">
-      <a class="active" @click="routeTo('/')">Home</a>
-      <a @click="routeTo('/#token')">Token</a>
-      <a @click="routeTo('/#contact')" id="conBtn">Contact Us</a>
-      <a @click="routeTo('about')">Our Story</a>
-    </div>
-  </aside>
 </template>
 
-<script setup>
-import { ref } from "vue";
-import router from "../router";
-// toggler component
-const isNavOpen = ref(true);
-function routeTo(x) {
-  router.push(x);
-  isNavOpen.value = true;
-}
-</script>
+<script setup></script>
 
 <style lang="scss" scoped>
-@import "../assets/variables.scss";
 nav {
-  background-color: #fff;
   width: 100%;
-  .content {
+  height: auto;
+  .top-info {
+    background-image: url(../assets/images/top-info.png);
+    width: 100%;
+    height: fit-content;
+    padding: 0.5rem 0px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    color: #fff;
+  }
+  .nav-content {
     width: 90%;
     margin: 0 auto;
+    height: fit-content;
     display: flex;
-    flex-direction: row;
     justify-content: space-between;
-    align-items: center;
-    // width: 80%;
-    margin: 0 auto;
-    padding: 0.75rem 0px;
-    position: relative;
-    z-index: 10;
-    #logo {
-      width: 48px;
-    }
-    .navMenu {
+    .logo,
+    .nav-list,
+    .buttons {
       display: flex;
-      flex-direction: row;
       align-items: center;
-      gap: 18px;
-      a {
+    }
+    .logo {
+      gap: 4px;
+      img {
+        width: 48px;
+        height: 48px;
+      }
+    }
+    .nav-list,
+    .buttons {
+      gap: 8px;
+      p {
         cursor: pointer;
-        text-decoration: none;
-        font-size: 14px;
-        color: inherit;
-      }
-      a:hover {
-        font-weight: 600;
-      }
-      a#conBtn {
-        background-color: #035a85;
-        color: #fff;
-        width: fit-content;
-        height: fit-content;
-        padding: 8px 18px;
-        border-radius: 4px;
-      }
-    }
-    .navToggler {
-      display: none;
-    }
-  }
-}
-aside {
-  display: none;
-}
-// Mobile responsiveness
-@media #{$media-tablet} {
-  nav {
-    width: 100%;
-  }
-}
-@media #{$media-mobile} {
-  nav {
-    position: sticky;
-    z-index: 10;
-    top: 0;
-    .content {
-      .navMenu {
-        display: none;
-      }
-      .navToggler {
-        display: block;
-        button {
-          background-color: #ffffff00;
-          border: none;
-        }
-      }
-    }
-  }
-  aside {
-    display: flex;
-    position: fixed;
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 100vh;
-    z-index: 5;
-    background: rgba(0, 0, 0, 0.7);
-    backdrop-filter: blur(2px);
-    color: #fff;
-    align-items: center;
-    .sidemenu {
-      width: fit-content;
-      background-color: #fff;
-      border-radius: 8px 0px 0px 8px;
-      height: fit-content;
-      color: #000;
-      display: flex;
-      flex-direction: column;
-      position: absolute;
-      right: 0;
-      padding: 1rem 0px;
-      a {
-        margin: 4px 4px;
-        padding: 16px;
-        font-size: 16px;
-        font-weight: 600;
-      }
-      a.active {
-        border-left: 2px solid #035a85;
-        font-weight: 600;
-        background-color: #035a852a;
       }
     }
   }
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,80 +1,14 @@
 <script setup>
 import NavBar from "../components/NavBar.vue";
-import QualityCon from "../components/QualityCon.vue";
-import ConvincerFlex from "../components/ConvincerFlex.vue";
-import TokenHoleder from "../components/TokenHoleder.vue";
-import TestimonialSect from "../components/TestimonialSect.vue";
-import FreqAsked from "../components/FreqAsked.vue";
-import ContactSect from "../components/ContactSect.vue";
-import SubSect from "../components/SubSect.vue";
-import FooterSect from "../components/FooterSect.vue";
-import { ref, onMounted } from "vue";
-// design variables
-const alertOpen = ref(false);
-const subscribeEmail = ref("");
-const subcribeModal = ref(false);
-// Close modal fuction
-function clearModal() {
-  alertOpen.value = false;
-  console.log(alertOpen);
-}
-onMounted(() => {
-  // set timeout to open subscribe email modal
-  setTimeout(() => {
-    alertOpen.value = true;
-    subcribeModal.value = true;
-  }, 10000);
-});
+// import { ref, onMounted } from "vue";
 </script>
 <template>
-  <!-- Pop up modals -->
-  <div class="alerts" v-if="alertOpen">
-    <div class="signUpModal" v-if="subcribeModal">
-      <img
-        id="clearIcon"
-        @click="clearModal"
-        src="../assets/images/modal_remove_icon.svg"
-        alt=""
-      />
-      <h3>Subscribe To our Emailing List</h3>
-      <input
-        type="email"
-        v-model="subscribeEmail"
-        placeholder="Email Address"
-      />
-      <button id="submit_btn" @click="submitEmail">Subscribe</button>
-    </div>
-  </div>
   <main>
-    <section class="hero">
-      <NavBar />
-      <div class="heroContent">
-        <div class="heroText">
-          <h3>ASSETAR EXCHANGE</h3>
-          <h1>Life is too short for your crypto transactions to take time</h1>
-          <p>
-            At asseter exchange we prioritize the speed of every customers
-            transaction
-          </p>
-          <a href="#" id="cta">Trade with Us</a>
-        </div>
-        <!-- <div class="heroImage">
-          <img src="../assets/images/hero_image.png" alt="" />
-        </div> -->
-      </div>
-    </section>
-    <QualityCon />
-    <ConvincerFlex />
-    <TokenHoleder id="token" />
-    <TestimonialSect />
-    <FreqAsked />
-    <ContactSect id="contact" />
-    <SubSect />
-    <FooterSect />
+    <NavBar />
+    <section class="hero"></section>
   </main>
 </template>
 <style lang="scss">
-@import "../assets/variables.scss";
 $media-desktop: "only screen and (max-width : 1024px)";
 $media-tablet: "only screen and (max-width : 768px)";
 $media-mobile: "only screen and (max-width : 600px)";
@@ -140,105 +74,14 @@ main {
   display: relative;
   overflow: hidden;
 }
-// Hero Section
-.hero {
-  background-image: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)),
-    url(../assets/images/asseter-hero.png);
-  background-size: 100% 150%;
-  background-position: center;
-  height: 100dvh;
-  width: 100%;
-  // display: flex;
-  // flex-direction: row;
-  // justify-content: flex-end;
-  .heroContent {
-    width: 90%;
-    display: grid;
-    height: 100%;
-    // flex-direction: row;
-    // align-items: center;
-    margin: 0 auto;
-    grid-template-columns: 50% 50%;
-    align-items: center;
-    justify-items: center;
-    .heroText {
-      h3 {
-        background: radial-gradient(
-            42.99% 42.99% at 30.18% 57.01%,
-            #444ce4 0%,
-            #037e82 100%
-          )
-          /* warning: gradient uses a rotation that is not supported by CSS and may not behave as expected */;
-        -webkit-background-clip: text;
-        -webkit-text-fill-color: transparent;
-        background-clip: text;
-        text-fill-color: transparent;
-      }
-      h1 {
-        margin: 1rem 0px;
-        color: #fff;
-      }
-      p {
-        color: #f2f2f2;
-        width: 60%;
-        margin-bottom: 2rem;
-      }
-      a#cta {
-        text-decoration: none;
-        padding: 12px 18px;
-        border-radius: 4px;
-        font-weight: 700;
-        background-color: #035a85;
-        color: #fff;
-        margin-top: 8px !important;
-      }
-    }
-    .heroImage {
-      width: 100%;
-      height: 80vh;
-      position: relative;
-      // overflow: hidden;
-      img {
-        width: 180%;
-        position: absolute;
-        top: -60%;
-        right: -40%;
-      }
-    }
-  }
+.nav {
+  position: fixed;
+  width: 180%;
+  left: 10%;
+  top: 2.5%;
+  z-index: 100;
 }
+// Hero Section
 @media #{$media-mobile} {
-  .hero {
-    background-size: 200% 100%;
-    .heroContent {
-      display: flex;
-      // flex-direction: column-reverse;
-      flex-direction: column;
-      justify-content: center;
-      width: 90%;
-      height: 100%;
-      margin: 0 auto;
-      .heroText {
-        // height: 100%;
-        h1 {
-          font-size: 24px;
-          font-weight: 700;
-        }
-        p {
-          width: 90%;
-        }
-      }
-      .heroImage {
-        // height: 45vh;
-        display: none;
-        img {
-          position: relative;
-          width: 150%;
-          top: -40%;
-          right: 25%;
-        }
-      }
-    }
-  }
 }
 </style>
