896889c98ed2c7261c83057aa850413d37a8e610
Okeibunor Emmanuel <emmanuel@numinix.com>
2025-03-07 00:53:22 +0100

    feat:: social proof

--- src/components/socialProof.vue
+++ src/components/socialProof.vue
@@ -1,13 +1,24 @@
 <template>
   <section>
-    <h4 class="intro">We trade with these platforms</h4>
+    <h4 class="intro">We actively trade on these platforms</h4>
     <div class="image-row">
-      <img src="../assets/images/svg/access.svg" alt="" />
-      <img src="../assets/images/svg/barter.svg" alt="" />
-      <img src="../assets/images/svg/cowerywise.svg" alt="" />
-      <img src="../assets/images/svg/ehealth.svg" alt="" />
-      <img src="../assets/images/svg/farmcrowdy.svg" alt="" />
-      <img src="../assets/images/svg/uba.svg" alt="" />
+      <img
+        src="https://cdn.brandfetch.io/idFbd86e4i/w/820/h/321/theme/light/logo.png?c=1dxbfHSJFAPEGdCLU4o5B"
+        alt=""
+        style="background-color: black; border-radius: 8px"
+      />
+      <img
+        src="https://cdn.brandfetch.io/id-pjrLx_q/theme/dark/logo.svg?c=1dxbfHSJFAPEGdCLU4o5B"
+        alt=""
+      />
+      <img
+        src="https://cdn.brandfetch.io/idwDWo4ONQ/theme/dark/logo.svg?c=1dxbfHSJFAPEGdCLU4o5B"
+        alt=""
+      />
+      <img
+        src="https://cdn.brandfetch.io/id_HKIytUb/theme/dark/logo.svg?c=1dxbfHSJFAPEGdCLU4o5B"
+        alt=""
+      />
     </div>
   </section>
 </template>
@@ -37,6 +48,7 @@ section {
   img {
     aspect-ratio: 16/4;
     object-fit: contain;
+    width: 150px;
   }
   @media #{$media-mobile} {
     .image-row {
