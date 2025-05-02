8d7a1068e1b190e3585d78309da945d88df19838
Okeibunor Emmanuel <emmanuel@numinix.com>
2025-05-02 01:50:59 +0100

    legacy build to fix snap chat issue

--- vite.config.js
+++ vite.config.js
@@ -2,13 +2,20 @@ import { fileURLToPath, URL } from "node:url";
 
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import legacy from "@vitejs/plugin-legacy";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
     target: "es2015",
   },
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    legacy({
+      // include any targets needed (defaults covers recent browsers and iOS>=12)
+      targets: ["defaults", "not IE 11"],
+    }),
+  ],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
